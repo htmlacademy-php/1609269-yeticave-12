00eb6e9dde6a0c4e1e8e0e5feb5b1b7b91783b15
apgrayedd <56434676+apgrayedd@users.noreply.github.com>
2020-11-23 10:23:02 +0300

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,18 +1,38 @@
 CREATE DATABASE yeticave;
 USE yeticave;
 
-CREATE TABLE products
-(
-   ID INT NOT NULL PRIMARY KEY,
-   name_pr char(50) NOT NULL,
-   category_pr char(50) NOT NULL,
-   price_pr int NOT NULL,
-   img_link_pr char(50) NOT NULL,
-   date_pr int NOT NULL
-);
-
 CREATE TABLE categorys
 (
-   ID INT NOT NULL PRIMARY KEY,
-   categorys_pr char(50) NOT NULL
+   categorys_ID INT NOT NULL PRIMARY KEY,
+   category char(50) NOT NULL
+);
+
+CREATE TABLE lot
+(
+   lot_ID INT NOT NULL PRIMARY KEY,
+   lot_date_create int not null,
+   lot_name char(50) not null,
+   lot_description char(50) not null,
+   lot_img_link char(50) not null,
+   lot_start_price int not null,
+   lot_date_completion int not null,
+   lot_step_rate int not null
+);
+
+CREATE TABLE rate
+(
+   rate_ID INT NOT NULL PRIMARY KEY,
+   rate_date_create int not null,
+   rate_price int not null
+);
+
+CREATE TABLE users
+(
+   users_ID INT NOT NULL PRIMARY KEY,
+   users_date_create int not null,
+   users_email char(50) not null UNIQUE,
+   users_name char(50) not null UNIQUE,
+   user_password char(50) not null,
+   user_email char(50) not null,
+   users_contact int not null
 );
