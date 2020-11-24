8c45090cb96e7a11fe15a1c4da67dea27432d4af
apgrayedd <56434676+apgrayedd@users.noreply.github.com>
2020-11-24 16:37:56 +0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,22 +1,22 @@
 INSERT INTO categories VALUES 
-(1,'Доски и лыжи',0),
-(2,'Крепления',1),
-(3,'Ботинки',2),
-(4,'Одежда',3),
-(5,'Инструменты',4),
-(6,'Разное',5);
+(1,'Доски и лыжи','boards'),
+(2,'Крепления','attachment'),
+(3,'Ботинки','boots'),
+(4,'Одежда','clothing'),
+(5,'Инструменты','tools'),
+(6,'Разное','other');
 
 INSERT INTO users VALUES
 (1,'2020-11-23','yandex@mail.ru','yandex','$2y$10$NvdjJOFXly/ybiO1IwSMre0x0mraCebAnGNxe8RJC27yMn4P/vSfK','+7988'),
 (2,'2020-11-23','google@mail.ru','google','$2y$10$hOtl9f4C3VcFdXkBGF.EH.j9cdbnZ3noct/mWGwGjaQryzY3qkFxO','+7989');
 
 INSERT INTO lots VALUES
-(1,'2020-11-23','2014 Rossignol District Snowboard','',1,0,0,'img/lot-1.jpg',10999,'2020-11-25',15000),
-(2,'2020-11-23','DC Ply Mens 2016/2017 Snowboard','',1,0,0,'img/lot-2.jpg',159999,'2020-12-23',200000),
-(3,'2020-11-23','Крепления Union Contact Pro 2015 года размер L/XL','',1,2,1,'img/lot-3.jpg',8000,'2020-11-24',8500),
-(4,'2020-11-23','Ботинки для сноуборда DC Mutiny Charocal','',1,2,2,'img/lot-4.jpg',10999,'2020-11-23',10999),
-(5,'2020-11-23','Куртка для сноуборда DC Mutiny Charocal','',1,0,3,'img/lot-5.jpg',7500,'2020-11-27',7700),
-(6,'2020-11-23','Маска Oakley Canopy','',1,2,5,'img/lot-6.jpg',5400,'2020-11-23',6000);
+(1,'2020-11-23','2014 Rossignol District Snowboard','',1,0,1,'img/lot-1.jpg',10999,'2020-11-25',15000),
+(2,'2020-11-23','DC Ply Mens 2016/2017 Snowboard','',1,0,1,'img/lot-2.jpg',159999,'2020-12-23',200000),
+(3,'2020-11-23','Крепления Union Contact Pro 2015 года размер L/XL','',2,1,2,'img/lot-3.jpg',8000,'2020-11-24',8500),
+(4,'2020-11-23','Ботинки для сноуборда DC Mutiny Charocal','',1,2,3,'img/lot-4.jpg',10999,'2020-11-23',10999),
+(5,'2020-11-23','Куртка для сноуборда DC Mutiny Charocal','',1,0,4,'img/lot-5.jpg',7500,'2020-11-27',7700),
+(6,'2020-11-23','Маска Oakley Canopy','',1,2,6,'img/lot-6.jpg',5400,'2020-11-23',6000);
 
 INSERT INTO bids VALUES
 (1,'2020-11-23',12999,1,1),
@@ -28,7 +28,7 @@ SELECT category FROM categories;
 SELECT lots.id ,name, start_price, img_link, step_rate, category
 FROM lots
 JOIN categories
-ON lots.category_id = categories.code
+ON lots.category_id = categories.id
 WHERE lots.date_completion >= NOW()
 ORDER BY lots.date_create DESC; 
 /*получить самые новые, открытые лоты. Каждый лот должен включать название, стартовую цену, ссылку на изображение, текущую цену, название категории;*/
