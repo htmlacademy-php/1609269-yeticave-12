cc29d776791ca5af6072ddf69dfa7429314744ef
apgrayedd <56434676+apgrayedd@users.noreply.github.com>
2020-11-24 16:57:17 +0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -11,12 +11,12 @@ INSERT INTO users VALUES
 (2,'2020-11-23','google@mail.ru','google','$2y$10$hOtl9f4C3VcFdXkBGF.EH.j9cdbnZ3noct/mWGwGjaQryzY3qkFxO','+7989');
 
 INSERT INTO lots VALUES
-(1,'2020-11-23','2014 Rossignol District Snowboard','',1,0,1,'img/lot-1.jpg',10999,'2020-11-25',15000),
-(2,'2020-11-23','DC Ply Mens 2016/2017 Snowboard','',1,0,1,'img/lot-2.jpg',159999,'2020-12-23',200000),
-(3,'2020-11-23','Крепления Union Contact Pro 2015 года размер L/XL','',2,1,2,'img/lot-3.jpg',8000,'2020-11-24',8500),
-(4,'2020-11-23','Ботинки для сноуборда DC Mutiny Charocal','',1,2,3,'img/lot-4.jpg',10999,'2020-11-23',10999),
-(5,'2020-11-23','Куртка для сноуборда DC Mutiny Charocal','',1,0,4,'img/lot-5.jpg',7500,'2020-11-27',7700),
-(6,'2020-11-23','Маска Oakley Canopy','',1,2,6,'img/lot-6.jpg',5400,'2020-11-23',6000);
+(1,'2020-11-23','2014 Rossignol District Snowboard','',1,0,1,'img/lot-1.jpg',10999,'2020-11-25',200),
+(2,'2020-11-23','DC Ply Mens 2016/2017 Snowboard','',1,0,1,'img/lot-2.jpg',159999,'2020-12-23',200),
+(3,'2020-11-23','Крепления Union Contact Pro 2015 года размер L/XL','',2,1,2,'img/lot-3.jpg',8000,'2020-11-24',200),
+(4,'2020-11-23','Ботинки для сноуборда DC Mutiny Charocal','',1,2,3,'img/lot-4.jpg',10999,'2020-11-23',200),
+(5,'2020-11-23','Куртка для сноуборда DC Mutiny Charocal','',1,0,4,'img/lot-5.jpg',7500,'2020-11-27',200),
+(6,'2020-11-23','Маска Oakley Canopy','',1,2,6,'img/lot-6.jpg',5400,'2020-11-23',200);
 
 INSERT INTO bids VALUES
 (1,'2020-11-23',12999,1,1),
