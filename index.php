c3debd7694a11e07ebd3ada40a931d216822551b
apgrayedd <56434676+apgrayedd@users.noreply.github.com>
2020-12-04 13:44:03 +0300

    indentation

--- index.php
+++ index.php
@@ -2,23 +2,23 @@
 include(__DIR__.'/bootstrap.php');
 
 $select_categories = 
-"SELECT categories.* 
-FROM categories";
+    "SELECT categories.* 
+    FROM categories";
 $select_lots = 
-"SELECT lots.id ,name,start_price,img_link,
-MAX(COALESCE(bids.price,lots.start_price)) AS price, 
-date_completion ,category
+    "SELECT lots.id ,name,start_price,img_link,
+    MAX(COALESCE(bids.price,lots.start_price)) AS price, 
+    date_completion ,category
 
-FROM lots
-LEFT JOIN bids
-ON lots.id = bids.lot_id
+    FROM lots
+    LEFT JOIN bids
+    ON lots.id = bids.lot_id
 
-LEFT JOIN categories
-ON lots.id = categories.id
+    LEFT JOIN categories
+    ON lots.id = categories.id
 
-WHERE lots.date_completion >= NOW()
-GROUP BY lots.id
-ORDER BY lots.date_create DESC;";
+    WHERE lots.date_completion >= NOW()
+    GROUP BY lots.id
+    ORDER BY lots.date_create DESC;";
 
 $products = mysqli_fetch_all(mysqli_query($con,$select_lots),MYSQLI_ASSOC);
 $categorys = mysqli_fetch_all(mysqli_query($con,$select_categories),MYSQLI_ASSOC);
--- lot.php
+++ lot.php
@@ -4,29 +4,30 @@ include(__DIR__.'/bootstrap.php');
 $id = $_GET['id'];
 
 $select_categories = 
-"SELECT categories.* 
-FROM categories";
+    "SELECT categories.*     
+    FROM categories";
 $select_lots = 
-"SELECT lots.id ,name,start_price,img_link,
-MAX(COALESCE(bids.price,lots.start_price)) AS price, 
-date_completion ,category,description, MAX(COALESCE(bids.price,lots.start_price)) + step_rate AS min_bid
+    "SELECT lots.id ,name,start_price,img_link,
+    MAX(COALESCE(bids.price,lots.start_price)) AS price, 
+    date_completion ,category,description, 
+    MAX(COALESCE(bids.price,lots.start_price)) + step_rate AS min_bid
 
-FROM lots
-LEFT JOIN bids
-ON lots.id = bids.lot_id
+    FROM lots
+    LEFT JOIN bids
+    ON lots.id = bids.lot_id
 
-LEFT JOIN categories
-ON lots.id = categories.id
+    LEFT JOIN categories
+    ON lots.id = categories.id
 
-WHERE lots.id = ?
-GROUP BY lots.id
-ORDER BY lots.date_create DESC;";
+    WHERE lots.id = ?
+    GROUP BY lots.id
+    ORDER BY lots.date_create DESC;";
 $select_bids = 
-"SELECT bids.date_create, bids.price ,users.name
-FROM bids
-JOIN users
-ON users.id = bids.user_id
-WHERE bids.lot_id = ?
+    "SELECT bids.date_create, bids.price ,users.name
+    FROM bids
+    JOIN users
+    ON users.id = bids.user_id
+    WHERE bids.lot_id = ?
 ORDER BY bids.date_create DESC;";
 
 $products_query = replace_in_query($select_lots,$con,$id);
