fd4e315edea59836f833b777f9e16977b3fced74
apgrayedd <56434676+apgrayedd@users.noreply.github.com>
2021-01-08 14:45:02 +0300

    little update(2nd)

--- sign-up.php
+++ sign-up.php
@@ -6,7 +6,9 @@ if(isset($_SESSION['user']['name'])){
 $errors = [];
 if($_SERVER['REQUEST_METHOD'] == 'POST'){
     $errors['email'] = check_input('email',1,320,FILTER_VALIDATE_EMAIL);
-    $errors['email'] = (empty($errors['email']) and select_user_by_email($_POST['email'],$con))?"Аккаунт с таким email уже есть!":$errors['email'];
+    if(!$errors['email'] and select_user_by_email($_POST['email'],$con)){
+        $errors['email'] = "Аккаунт с таким email уже есть!";
+    }
     $errors['password'] = check_input('password',8,200);
     $errors['name'] = check_input('name',1,30);
     $errors['message'] = check_input('message',0,2000);
