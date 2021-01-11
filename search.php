4ec140ecdc18e05cdf5659451080634f37d7d74c
apgrayedd <56434676+apgrayedd@users.noreply.github.com>
2021-01-11 22:38:51 +0300

    Update search.php

--- search.php
+++ search.php
@@ -13,27 +13,32 @@ for($i = 0;$i < count($search);$i++){
     $search[$i] = $search[$i]."*";
 }
 $search_query = 
-"SELECT *,category, MAX(COALESCE(bids.price,lots.start_price)) AS price
+"SELECT COUNT(*) as count
  FROM lots
-
- LEFT JOIN bids
- ON lots.id = bids.lot_id
-
- LEFT JOIN categories
- ON lots.category_id = categories.id
-
- WHERE MATCH(lots.name, lots.description) AGAINST(? IN BOOLEAN MODE)".str_repeat(
- " AND MATCH(lots.name, lots.description) AGAINST(? IN BOOLEAN MODE)",count($search) - 1).
- " GROUP BY lots.id";
-
+ WHERE lots.id IN(
+    SELECT lots.id
+    FROM lots
+    WHERE MATCH(lots.name, lots.description) AGAINST(? IN BOOLEAN MODE)".str_repeat(
+    " AND MATCH(lots.name, lots.description) AGAINST(? IN BOOLEAN MODE)",count($search) - 1).
+    " GROUP BY lots.id)";
 $founding_lots = prepared_query($search_query,$con,$search)->get_result();
-$lots = mysqli_fetch_all($founding_lots,MYSQLI_ASSOC);
-$count_page = ceil(count($lots)/$_GET['limit']);
+$count_lots = mysqli_fetch_assoc($founding_lots);
+$count_page = ceil($count_lots['count']/$_GET['limit']);
 if($_GET['page'] > $count_page){
     page_404($categorys);
 }
-
-$search_with_limit = $search_query." LIMIT ? OFFSET ?";
+$search_with_limit = 
+"SELECT *,category, MAX(COALESCE(bids.price,lots.start_price)) AS price
+ FROM lots
+ LEFT JOIN bids
+ ON lots.id = bids.lot_id
+ LEFT JOIN categories
+ ON lots.category_id = categories.id
+ WHERE MATCH(lots.name, lots.description) AGAINST(? IN BOOLEAN MODE) ".str_repeat(
+ " AND MATCH(lots.name, lots.description) AGAINST(? IN BOOLEAN MODE) ",count($search) - 1).
+ "GROUP BY lots.id
+  LIMIT ? 
+  OFFSET ?";
 $founding_lots_limit = prepared_query($search_with_limit,$con,array_merge($search,[$_GET['limit'],($_GET['page']-1)*$_GET['limit']]))->get_result();
 $lots = mysqli_fetch_all($founding_lots_limit,MYSQLI_ASSOC);
 
