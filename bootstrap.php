f834527e9293f8ad16f30d23af2dff802e044758
apgrayedd <56434676+apgrayedd@users.noreply.github.com>
2021-01-18 12:15:35 +0300

    add errors

--- bootstrap.php
+++ bootstrap.php
@@ -6,16 +6,17 @@ require __DIR__."/config.php";
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 ini_set('display_errors',1);
 error_reporting(E_ALL);
-$con = mysqli_connect($db_host,$db_name,$db_password,$db_database);
-mysqli_set_charset($con, "utf8mb4");
-
 if (ini_get('display_errors')) {
-    set_exception_handler(function ($e){
-        error_page(500);
-    });
+    set_exception_handler(
+        function ($e){
+            error_page(500);
+        });
 }
 
+$con = mysqli_connect($db_host,$db_name,$db_password,$db_database);
+mysqli_set_charset($con, "utf8mb4");
 session_start();
+
 //Создние якоря для переадресация пользователя на последнюю открытую им страницу перед логином или регистрацией
 $_SESSION['link'] = (!isset($_SESSION['link'])) ? "index.php" : $_SESSION['link'];
 $_SESSION['link'] = (in_array($_SERVER['REQUEST_URI'],["/login.php","/sign-up.php","/logout.php"]))? $_SESSION['link']:$_SERVER['REQUEST_URI'];
@@ -33,7 +34,7 @@ $sql_query =
    WHERE lot_id = lots.id
    ORDER BY price DESC
    limit 1),user_id)
- WHERE date_completion < NOW() AND winner_id IS NULL";
+ WHERE date_completion <= NOW() AND winner_id IS NULL";
 $stmt = $con -> prepare($sql_query);
 $stmt->execute();
 
